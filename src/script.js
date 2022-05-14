3f7b923da979520727d00b229ad5bd8a19b0154e
Prajna1999 <gituprajna20@gmail.com>
2022-05-14 16:49:49 +0530

    basic functionality

--- src/script.js
+++ src/script.js
@@ -4,6 +4,23 @@ let movies=[];
 
 function buildList(){
     document.querySelector("nav>h2").innerHTML=`${movies.length} star war movies are found.`
+
+    const main=document.querySelector("main");
+    main.innerHTML=movies.map((m)=>{
+        return `<div class="movie" data-ref="${m.id}" data-episode="${m.episode_id}">
+                <h2>Star Wars:${m.release_date} </h2>
+                <p>Directed by: ${m.director}</p>
+                <p>Random Charachter:
+                    <span class="char">${m.characters[0]}</span>
+                </p>
+                <p><a href="${m.url}" target="_blank">More Info </a></p>
+                </div>
+
+        
+        
+        
+        `
+    }).join('');
 }
 
 function init(){
